4a9f0b941581a8c75d91a1be16c0d13d586c8f46
Mochamad Farhan Abdullah <112877057+MochamadFarhanAbdullah@users.noreply.github.com>
2023-03-09 08:17:53 +0700

    Latihan fungsi menggunakan codeblock

---
+++ Latihan_fungsi_codeblock/main.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+
+int jumlah(int a, int b);
+int selisih(int a, int b);
+int kali(int a, int b);
+float bagi(float a, float b);
+
+int main()
+{
+    int a, b;
+    cout << "Masukkan angka : ";
+    cin >> a;
+    cout << "Masukkan angka : ";
+    cin >> b;
+    cout << jumlah(a, b) << endl;
+    cout << selisih(a, b) << endl;
+    cout << kali(a, b) << endl;
+    cout << bagi(a, b) << endl;
+
+    return 0;
+}
+
+int jumlah(int a, int b)
+{
+    return a + b;
+}
+
+int selisih(int a, int b)
+{
+    return a - b;
+}
+
+int kali(int a, int b)
+{
+    return a * b;
+}
+
+float bagi(float a, float b)
+{
+    float x = (float)a / b;
+    return x;
+}
